64639b3c0cd24b3d1a9df6855747362076ddb9a5
Sam Ridding <sam@sams-air.home>
2023-05-02 22:03:15 +0100

    Add post list view and url path

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import generics, permissions
+from .models import Post
+from .serializers import PostSerializer
 
-# Create your views here.
+
+class PostList(generics.ListCreateAPIView):
+
+    serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = Post.objects.all().order_by('-posted_at')
+    filter_backends = [
+        DjangoFilterBackend
+    ]
+    filterset_fields = [
+        'author'
+    ]
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
