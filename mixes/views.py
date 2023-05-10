1634c5f55ec6bbcd1beae8ce93073c29e09a68da
Sam Ridding <sam@sams-air.home>
2023-05-10 21:16:53 +0100

    Add list and detail views for mixes app

--- mixes/views.py
+++ mixes/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from rest_framework import generics, permissions
+from .models import Mix
+from .serializers import MixSerializer
 
-# Create your views here.
+
+class MixList(generics.ListCreateAPIView):
+
+    serializer_class = MixSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = Mix.objects.all().order_by('-posted_at')
+
+    def perform_create(self, serializer):
+        serializer.save()
+
+
+class MixDetail(generics.RetrieveUpdateDestroyAPIView):
+
+    serializer_class = MixSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = Mix.objects.all().order_by('-posted_at')
